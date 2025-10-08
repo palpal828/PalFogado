f4c47d7164ab293f1079cd53b915b06b91d956db
Pallaga Pál <115153237+palpal828@users.noreply.github.com>
2025-10-08 10:12:50 +0200

    frontend temp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 
 import './App.css'
 
@@ -7,7 +7,29 @@ function App() {
 
   return (
     <>
-      
+      <div>
+        <div>
+            <div>
+              <h3>Napraforgós Nemzeti Tanúsító Védjegy célja</h3>
+                A falusi szálláshelyek napraforgós Nemzeti Tanúsító Védjegye a FATOSZ által több mint tíz éve létrehozott, és működtetett minősítési rendszer alapjaira épülve 2011 óta a minőségi falusi turizmus szimbóluma. A védjegy alapvető célja, hogy – összhangban az egyes szálláshelyek működtetéséről szóló 239/2009. Korm. rendeletben foglaltakkal – garanciát nyújtson a szálláshely szolgáltatás minőségének megfelelő színvonalára.  A falusi vendégházak 1-4 napraforgós besorolást nyerhetnek el a külső, belső megjelenés, a felszereltség, a szolgáltatások színvonala, valamint a szállásadó személyes felkészültségének, szakmai képzettségének függvényében. 
+                    <p></p>
+                    <p></p>
+            </div>
+            <div>
+                <h3>Falusi szálláshely fajtái</h3>
+
+                      <p>
+                      </p>
+                      <p>
+
+                      </p>
+            </div>
+            <div>
+              <h3>A hét törpe fogadó</h3>
+
+            </div>
+        </div>
+      </div>
     </>
   )
 }
